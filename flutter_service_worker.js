a8342244acd8a86855aa97fc0126e519514f9236
guilherme <gguilhermepires@gmail.com>
2022-05-12 10:37:26 -0300

    atualizando env

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,8 +81,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "df72540508834cd7f2dd7f4985cee794",
-"/": "df72540508834cd7f2dd7f4985cee794",
+"index.html": "4e75640ed667779f62a4786693e5c5c2",
+"/": "4e75640ed667779f62a4786693e5c5c2",
 "main.dart.js": "3e83c65cdb53eea8293e7fb0b83d3355",
 "manifest.json": "3ac59c144a947293178b6b4ae68cb7ea",
 "version.json": "d7210237fd673738848cbb7dff7f6edc"
